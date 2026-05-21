4f661025e7fba248fa4e278995bfef5673a47281
CubicKim <rlatpgns1019@cubicinc.co.kr>
2026-05-21 14:54:40 +0900

    배포 확인

--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         /*return "<h1>[IntelliJ-Community] 순수 Spring Boot (Java 1.8) 배포 성공! - Version 1</h1>";*/
-        return "<h1>[IntelliJ-Community] 순수 Spring Boot (Java 17) 배포 성공! - Version 2</h1>";
+        /*return "<h1>[IntelliJ-Community] 순수 Spring Boot (Java 17) 배포 성공! - Version 2</h1>";*/
+        return "<h1>배포 확인 - Version 3</h1>";
     }
 }
